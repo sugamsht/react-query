7c9bb86fc117f2df1e8b111bbd316c400c311fe3
Sugam Shrestha <sugamsht143@gmail.com>
2022-03-15 00:09:51 +0545

    implement cacheTime, staleTime, refetches

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,13 +1,29 @@
 import axios from "axios"
 import { useQuery } from "react-query"
 
+const fetchSuperHeros = () => {
+  return axios.get("http://localhost:4000/superheroes")
+}
+
 export const RQSuperHeroesPage = () => {
-  useQuery("superHeroes", () => {
-    return axios.get("http://localhost:4000/superheroes")
-  })
-  const { data, error, isLoading } = useQuery("superHeroes")
+
+  const { data, error, isLoading, isFetching } = useQuery(
+    'superheroes',
+    fetchSuperHeros,
+    {
+      // cacheTime: 5000, // 5 seconds default:5 minutes
+      // staleTime: 30000, //default: 0 seconds
+      // refetchOnMount: true, //default:true refetches if data is stale 'always':always refetches
+      // refetchOnWindowFocus: true, //default:true refetches if window is focused
+      refetchInterval: 10000, //polling refetch data default:false
+      // refetchIntervalInBackground: true, //polling refetch data even if browser is in background default:false
+    }
+  )
+
+  console.log({ isLoading, isFetching })
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>{error.message}</div>
+
   return (
     <div>
       <h1>RQ Super Heroes</h1>
